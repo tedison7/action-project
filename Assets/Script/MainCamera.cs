0ac9859c0a461855d8df97ea90e8274d311884d9
tedison <ktjiss0930@gmail.com>
2021-08-24 22:41:35 +0900

    first commit

---
+++ Assets/Script/MainCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Vector3 CameraOffset;
+
+    void Update()
+    {
+        transform.position = targetTransform.position + CameraOffset;
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody charRigidbody;
+    private Animator charAnimator;
+
+    void Start()
+    {
+        charRigidbody = GetComponent<Rigidbody>();
+        charAnimator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDir = new Vector3(hAxis, 0, vAxis).normalized;
+
+        charRigidbody.velocity = inputDir * moveSpeed;
+
+        transform.LookAt(transform.position + inputDir);
+
+        charAnimator.SetBool("IsRun", inputDir != Vector3.zero);
+    }
+}
